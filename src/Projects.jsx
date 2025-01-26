05525ba8238c9c408159863ec0e5938f67ca2af4
Aladin Cacho <cachoaladin192@gmail.com>
2025-01-26 22:08:23 +0800

    Add projects

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,90 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import { Link } from 'react-scroll';
-import servIcon from './files/portfolio.png';
-import shoescenter from './files/1.png';
-import philtrip from './files/2.png';
-import examalter from './files/3.png';
-import pv1 from './files/5.png';
-import p6 from './files/6.png';
-import p7 from './files/7.png';
-import p8 from './files/8.png';
-import { FaCircle } from "react-icons/fa";
-function Projects() {
-    return (
-        <>
-            <div id="projects"className="bg-white w-full min-h-screen py-[80px] px-[150px]">
-                <div className="flex">
-                    {/* Sidebar navigation */}
-                    <div className="flex w-1/2   h-screen sticky top-0" >
-                        <div className="flex flex-col  justify-center items-center w-full " >
-                            <div className='bg-white h-[600px]'>
-                                <p className='text-[#FFBD59] font-semibold tracking-wide text-[60px] w-full '>
-                                    PORTFOLIO
-                                </p>
-                                <p>The sites on your right are the best bits which is built from scratch.</p>
-                            
-                                <img src={servIcon}width="450px" alt=""className='mt-3 ' />    
-                                <p className='text-xl tracking-widest mb-2'>All the projects on your right are built from scratch in the following stack:</p>
-                                <div className="flex justify-start items-center text-[#FFBD59] ">
-                                     <FaCircle className="mr-2 text-[11px]"/> Html  <FaCircle className="m-2 text-[11px]"/>  Css  <FaCircle className="m-2 text-[11px]"/>  Javascript  <FaCircle className="m-2 text-[11px]"/>  Wordpress  <FaCircle className="m-2 text-[11px]"/>  PHP   <FaCircle className="m-2 text-[11px]"/>  Java.
-                                </div>
-                            </div>                        
-                        </div>
-                    </div>
-
-                    {/* Content section */}
-                    <div className="w-1/2 ml-10 pt-[86px] mb-12">
-                        
-                        <div className="  mb-10 pb-5 " data-aos="fade-up">
-                            <img src={examalter} alt="" width="600px"  className=' border rounded-2xl shadow-xl mx-auto'/>
-                            <h1 id="examalter" className='mt-5 text-left text-4xl font-semibold text-[#FFBD59] mb-2'>EXAMALTER</h1>
-                            <p className=' text-left text-base text-[#979393] '>A web application designed for teachers to shuffle questionnaires, ensuring unique question orders for each student. Developed to help minimize cheating by offering dynamic quiz generation.</p>
-                        </div>
-
-                        <div className="  mb-10 pb-5 " data-aos="fade-up">
-                            <img src={shoescenter} alt="" width="600px"  className=' border rounded-2xl shadow-xl mx-auto'/>
-                            <h1 id="examalter" className='mt-5 text-left text-2xl font-semibold text-[#FFBD59]'>SHOES CENTER</h1>
-                            <p className=' text-left text-base text-[#979393] '>An eCommerce website for a shoe store that allows customers to browse and select shoes. Payments are made through an alternative method where users upload a GCash receipt for manual verification, ensuring a secure and flexible transaction process without a full payment gateway integration.</p>
-                        </div>
-
-                        <div className="  mb-10 pb-5 " data-aos="fade-up">
-                            <img src={philtrip} alt="" width="600px"  className=' border rounded-2xl shadow-xl mx-auto'/>
-                            <h1 id="examalter" className='mt-5 text-left text-2xl font-semibold text-[#FFBD59]'>PHILTRIP</h1>
-                            <p className=' text-left text-base text-[#979393] '>A travel booking web application where users can explore destinations and book trips. Although no payment gateway is implemented, the system facilitates booking requests and manages user inquiries about travel packages.</p>
-                        </div>
-
-                        <div className="  mb-10 pb-5 " data-aos="fade-up">
-                            <img src={pv1} alt="" width="600px"  className=' border rounded-2xl shadow-xl mx-auto'/>
-                            <h1 id="examalter" className='mt-5 text-left text-2xl font-semibold text-[#FFBD59]'>PORTFOLIO</h1>
-                            <p className=' text-left text-base text-[#979393] '>A personal portfolio website created using WordPress, showcasing my projects, skills, and professional background. Designed to be visually appealing and user-friendly, reflecting my work as a web developer.</p>
-                        </div>
-                        <div className="  mb-10 pb-5 " data-aos="fade-up">
-                            <img src={p6} alt="" width="600px"  className=' border rounded-2xl shadow-xl mx-auto'/>
-                            <h1 id="examalter" className='mt-5 text-left text-2xl font-semibold text-[#FFBD59]'>INSTRUCTOR MANAGEMENT SYSTEM</h1>
-                            <p className=' text-left text-base text-[#979393] '>A Java-based desktop application for managing instructor information and schedules. Provides an intuitive interface for tracking instructor availability, assignments, and performance monitoring.</p>
-                        </div>
-                        <div className="  mb-10 pb-7 " data-aos="fade-up">
-                            <img src={p7} alt="" width="600px"  className=' border rounded-2xl shadow-xl mx-auto'/>
-                            <h1 id="examalter" className='mt-5 text-left text-2xl font-semibold text-[#FFBD59]'>OFFICE SUPPLY MONITORING SYSTEM</h1>
-                            <p className=' text-left text-base text-[#979393] '>A desktop-based inventory management system built with Java, focused on monitoring office supplies. Tracks inventory levels, usage, and supply restocking, making it easier for office administrators to manage resources efficiently.</p>
-                        </div>
-                        <div className="  mb-10 pb-5 " data-aos="fade-up">
-                            <img src={p8} alt="" width="600px"  className=' border rounded-2xl shadow-xl mx-auto'/>
-                            <h1 id="examalter" className='mt-5 text-left text-2xl font-semibold text-[#FFBD59]'>BUDGET MONITORING SYSTEM</h1>
-                            <p className=' text-left text-base text-[#979393] '>A desktop application built with Java that helps users track and manage their budgets. It features data input, real-time monitoring of expenses, and visual representation of financial performance.
-
-</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
-}
-
-export default Projects;
-=======
 import React from 'react';
 import { Link } from 'react-scroll';
 import servIcon from './files/portfolio.png';
@@ -96,6 +9,12 @@ import p8 from './files/8.png';
 import shoes from './files/shoes.gif';
 import philtrip from './files/philtrip.gif';
 import office from './files/office.gif';
+import examGif from './files/exam.gif';
+import portfolioGif from './files/portfolio.gif';
+import manganTayoGif from './files/mangan-tayo.gif';
+import java1Gif from './files/java1.gif';
+import java2Gif from './files/java2.gif';
+import ganapGif from './files/ganap.gif';
 import { FaCircle, FaHtml5, FaCss3Alt, FaJs, FaWordpress, FaPhp, FaJava } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 
@@ -163,7 +82,7 @@ function Projects() {
                                 <a href="" target="_blank" rel="noopener noreferrer" title="Link to GitHub repo">
                                     <FaGithub color="gray" size={35} className='absolute right-5 top-5 cursor-pointer' />
                                 </a>
-                                <img src={examalter} alt="" width="600px" className='mx-auto' />
+                                <img src={examGif} alt="" width="600px" className='mx-auto' />
                             </div>
                             <h1 id="examalter" className='mt-5 text-left text-4xl font-semibold text-[#FFBD59] mb-2'>EXAMALTER</h1>
                             <p className='text-left text-base text-[#979393]'>A web application designed for teachers to shuffle questionnaires, ensuring unique question orders for each student. Developed to help minimize cheating by offering dynamic quiz generation.</p>
@@ -192,7 +111,7 @@ function Projects() {
                                 <a href="" target="_blank" rel="noopener noreferrer" title="Link to GitHub repo">
                                     <FaGithub color="gray" size={35} className='absolute right-5 top-5 cursor-pointer' />
                                 </a>
-                                <img src={pv1} alt="" width="600px" className='mx-auto' />
+                                <img src={portfolioGif} alt="" width="600px" className='mx-auto' />
                             </div>
                             <h1 id="examalter" className='mt-5 text-left text-2xl font-semibold text-[#FFBD59]'>PORTFOLIO</h1>
                             <p className='text-left text-base text-[#979393]'>A personal portfolio website created using WordPress, showcasing my projects, skills, and professional background. Designed to be visually appealing and user-friendly, reflecting my work as a web developer.</p>
@@ -209,7 +128,7 @@ function Projects() {
                                 <a href="" target="_blank" rel="noopener noreferrer" title="Link to GitHub repo">
                                     <FaGithub color="gray" size={35} className='absolute right-5 top-5 cursor-pointer' />
                                 </a>
-                                <img src={p6} alt="" width="600px" className='mx-auto' />
+                                <img src={java1Gif} alt="" width="600px" className='mx-auto' />
                             </div>
                             <h1 id="examalter" className='mt-5 text-left text-2xl font-semibold text-[#FFBD59]'>INSTRUCTOR MANAGEMENT SYSTEM</h1>
                             <p className='text-left text-base text-[#979393]'>A Java-based desktop application for managing instructor information and schedules. Provides an intuitive interface for tracking instructor availability, assignments, and performance monitoring.</p>
@@ -225,7 +144,36 @@ function Projects() {
                                 <a href="" target="_blank" rel="noopener noreferrer" title="Link to GitHub repo">
                                     <FaGithub color="gray" size={35} className='absolute right-5 top-5 cursor-pointer' />
                                 </a>
-                                <img src={p8} alt="" width="600px" className='mx-auto' />
+                                <img src={java2Gif} alt="" width="600px" className='mx-auto' />
+                            </div>
+                            <h1 id="examalter" className='mt-5 text-left text-2xl font-semibold text-[#FFBD59]'>BUDGET MONITORING SYSTEM</h1>
+                            <p className='text-left text-base text-[#979393]'>A desktop application built with Java that helps users track and manage their budgets. It features data input, real-time monitoring of expenses, and visual representation of financial performance.</p>
+                            <div className="flex justify-start items-center mt-2">
+                                <FaJava className="mr-2 text-[#007396]" size={20} />
+                            </div>
+                        </div>
+
+                        //Blogs
+                        <div className="mb-10 pb-5" data-aos="fade-up">
+                            <div className='border rounded-2xl shadow-xl relative'>
+                                <a href="" target="_blank" rel="noopener noreferrer" title="Link to GitHub repo">
+                                    <FaGithub color="gray" size={35} className='absolute right-5 top-5 cursor-pointer' />
+                                </a>
+                                <img src={manganTayoGif} alt="" width="600px" className='mx-auto' />
+                            </div>
+                            <h1 id="examalter" className='mt-5 text-left text-2xl font-semibold text-[#FFBD59]'>BUDGET MONITORING SYSTEM</h1>
+                            <p className='text-left text-base text-[#979393]'>A desktop application built with Java that helps users track and manage their budgets. It features data input, real-time monitoring of expenses, and visual representation of financial performance.</p>
+                            <div className="flex justify-start items-center mt-2">
+                                <FaJava className="mr-2 text-[#007396]" size={20} />
+                            </div>
+                        </div>
+
+                        <div className="mb-10 pb-5" data-aos="fade-up">
+                            <div className='border rounded-2xl shadow-xl relative'>
+                                <a href="" target="_blank" rel="noopener noreferrer" title="Link to GitHub repo">
+                                    <FaGithub color="gray" size={35} className='absolute right-5 top-5 cursor-pointer' />
+                                </a>
+                                <img src={ganapGif} alt="" width="600px" className='mx-auto' />
                             </div>
                             <h1 id="examalter" className='mt-5 text-left text-2xl font-semibold text-[#FFBD59]'>BUDGET MONITORING SYSTEM</h1>
                             <p className='text-left text-base text-[#979393]'>A desktop application built with Java that helps users track and manage their budgets. It features data input, real-time monitoring of expenses, and visual representation of financial performance.</p>
@@ -241,4 +189,3 @@ function Projects() {
 }
 
 export default Projects;
->>>>>>> 53ed138e514e5d761bd41f53510f2601371bee9b
--- src/Services.jsx
+++ src/Services.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import { TbDeviceDesktopCode } from "react-icons/tb";
 import { FaFileCode } from "react-icons/fa";
@@ -39,45 +38,3 @@ function Services(){
 }
 
 export default Services;
-=======
-import React from 'react';
-import { TbDeviceDesktopCode } from "react-icons/tb";
-import { FaFileCode } from "react-icons/fa";
-function Services(){
-    return(
-        <>
-        <section id="services" className='bg-[#FFF5E9]  pt-[100px] '>
-        <h1 className='w-full tracking-wide text-center text-[#FFBD59] text-lg font-semibold'>SERVICES</h1>
-        <h1 className='w-full mt-1 text-center font-semibold text-3xl '><span className='border-b-4 border-[#FFBD59]'>AREA</span> OF EXPERTISE</h1>
-        
-        <div className='w-full flex justify-center  mt-7 text-center text-base pb-9'>
-            <div className='group hover:shadow-xl   w-1/5 border mx-3 p-4 rounded-lg ' data-aos="fade-right">
-                <div className='w-full text-center'>
-                    <TbDeviceDesktopCode className="bg-[#FFBD59] text-black p-2 rounded-full text-7xl mx-auto"/>
-                </div>
-                <p className="mt-4 font-semibold ">FRONTEND DEVELOPMENT</p>
-                <p className='leading-7 mt-3 p-3'>I specialize in crafting visually appealing and highly interactive user interfaces. Using modern technologies like HTML, CSS, JavaScript, and frameworks such as React, I build responsive websites that provide seamless user experiences across all devices. My goal is to turn creative ideas into functional, user-friendly applications.</p>
-            </div>
-           
-            <div className='w-1/5 border mx-3 p-4 hover:shadow-xl rounded-lg' data-aos="fade-up">
-                <div className='w-full text-center'>
-                    <FaFileCode className="bg-[#FFBD59] text-black p-2 rounded-full text-7xl mx-auto"/>
-                </div>
-                <p className=" mt-4 font-semibold">BACKEND DEVELOPMENT</p>
-                <p className='leading-7 mt-3 p-3'>With expertise in server-side development, I handle the core functionality that powers web applications. I manage databases, API integrations, and server-side logic to ensure efficient, secure, and scalable solutions. My skills include working with Node.js, PHP, Python, and databases like MySQL and MongoDB, allowing me to build robust backends that support dynamic applications.</p>
-            </div>
-            <div className=' w-1/5 border mx-3 p-4 hover:shadow-xl rounded-lg ' data-aos="fade-left">
-                <div className='w-full text-center'>
-                    <TbDeviceDesktopCode className="bg-[#FFBD59] text-black p-2 rounded-full text-7xl mx-auto"/>
-                </div>
-                <p className="mt-4 font-semibold">GRAPHICS DESIGN</p>
-                <p className='leading-7 mt-3 p-3'>As a creative graphics designer, I create visually engaging digital assets that enhance the aesthetic appeal of websites and applications. From logos to custom illustrations, I use tools like Adobe Photoshop, Illustrator, and Figma to design high-quality visuals that align with the brand identity and improve the user experience.</p>
-            </div>
-        </div>
-        </section>
-        </>
-    );
-}
-
-export default Services;
->>>>>>> 53ed138e514e5d761bd41f53510f2601371bee9b

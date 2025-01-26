22b615c7ab088bfadf2d6f0a0a532ad687c964e7
Aladin Cacho <cachoaladin192@gmail.com>
2025-01-26 23:27:13 +0800

    Describe the changes you made

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from 'react';
 
 function Navigation(){
@@ -8,15 +8,6 @@ function Navigation(){
     </>
     )
 }
-=======
-import React from 'react';
 
-function Navigation(){
-    return(
-    <>
-       <h1>Logo</h1> 
-    </>
-    )
-}
->>>>>>> 53ed138e514e5d761bd41f53510f2601371bee9b
+
 export default Navigation;
